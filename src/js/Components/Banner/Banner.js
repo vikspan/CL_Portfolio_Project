8280bdaca68f85183e36ff47f1613b0e997d2b25
vikspan <viktorija.span@gmail.com>
2025-08-05 19:22:00 +0200

    add banner

---
+++ src/js/Components/Banner/Banner.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./_banner.scss";
+import profile from "../../../assets/profile.png";
+
+const Banner = () => {
+  return (
+    <section className="banner">
+      <div className="banner__title">
+        <h1>
+          Hi 👋, <br />
+          My name is <br />
+          Pavan MG <br />
+          I build things for web
+        </h1>
+      </div>
+      <div className="banner__image">
+        <img src={profile} alt="Profile" />
+      </div>
+
+    </section>
+  )
+}
+
+export default Banner;
--- src/js/Pages/Home.js
+++ src/js/Pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../Components/Header/Header";
+import Banner from "../Components/Banner/Banner";
 
 const Home = () => {
   return (
     <>
       <Header type="header" />
-      <p>Homepage</p>
+      <Banner />
       <Header type="footer"/>
     </>
   )
