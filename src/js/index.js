dd63d62d03c8200137c1ab97f206c79c9f3f108f
vikspan <viktorija.span@gmail.com>
2025-08-05 15:35:19 +0200

    add scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./Components/App";
+import "../scss/main.scss";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
