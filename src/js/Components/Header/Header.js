ea016569a6a1084355f49ce34d39952a40d081c3
vikspan <viktorija.span@gmail.com>
2025-08-05 16:47:33 +0200

    add Menu

--- src/js/Components/Header/Header.js
+++ src/js/Components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Logo from "./Logo/Logo";
+import Menu from "./Menu/Menu";
 
 const Header = ({type}) => {
   return (
     <div className={type}>
       <Logo type={type + "__logo"}/>
+      <Menu items={
+        [
+          {title: "Home", url: "#"},
+          {title: "About", url: "#about"},
+          {title: "Tech Stack", url: "#stack"},
+          {title: "Projects", url: "#projects"},
+          {title: "Contact", url: "#contact"}
+        ]
+      } />
     </div>
   )
 }
