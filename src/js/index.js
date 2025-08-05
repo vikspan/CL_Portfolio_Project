3f374baefa867f190e864b839b61ff6686d7610d
vikspan <viktorija.span@gmail.com>
2025-08-05 13:39:32 +0200

    environment setup

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import App
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+root.render(<p>intial</p>);
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const entryPath = "src";
+
+module.exports = {
+    mode: "none",
+    entry: `./${entryPath}/js/index.js`,
+    devtool: "inline-source-map",
+    output: {
+        filename: "out.js",
+        path: path.resolve(__dirname, `${entryPath}/build`),
+        clean: true,
+    },
+    devServer: {
+        open: true,
+        hot: true,
+        static: [
+            {
+                directory: path.join(__dirname, entryPath),
+                publicPath: "/",
+                serveIndex: true,
+            },
+        ],
+        devMiddleware: {
+            writeToDisk: true,
+        },
+        compress: true,
+        port: 3001,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [['autoprefixer']],
+                            },
+                        },
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                }
+            }
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+        }),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: entryPath +"/index.html",
+            inject: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/style.css",
+        }),
+
+    ],
+};
