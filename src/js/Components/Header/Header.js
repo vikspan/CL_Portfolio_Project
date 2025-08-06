d514d2adc8d25c52787e39d4b4381fda33e95029
vikspan <viktorija.span@gmail.com>
2025-08-06 07:32:53 +0200

    add section and icons

--- src/js/Components/Header/Header.js
+++ src/js/Components/Header/Header.js
@@ -3,6 +3,9 @@ import Logo from "./Logo/Logo";
 import Menu from "./Menu/Menu";
 import "./_header.scss";
 import Socials from "./Socials/Socials";
+import gh from "../../../assets/github.svg";
+import tw from "../../../assets/twitter.svg";
+import ln from "../../../assets/linkedin.svg";
 
 const Header = ({ type }) => {
   return (
@@ -19,7 +22,11 @@ const Header = ({ type }) => {
               { title: "Contact", url: "#contact" }
             ]
           } />
-        <Socials type={type + "__nav"} />
+        <Socials type={type + "__nav"} items={[
+          {url: "#", src: gh, alt: "GitHub"},
+          {url: "#", src: tw, alt: "Twitter"},
+          {url: "#", src: ln, alt: "LinkedIn"}
+        ]} />
       </div>
     </div>
   )
--- src/js/Components/Header/Socials/Socials.js
+++ src/js/Components/Header/Socials/Socials.js
@@ -1,15 +1,12 @@
 import React from "react";
-import gh from "../../../../assets/github.svg";
-import tw from "../../../../assets/twitter.svg";
-import ln from "../../../../assets/linkedin.svg";
 import "./_socials.scss";
 
-const Socials = ({type}) => {
+const Socials = ({ type, items }) => {
   return (
     <div className={type + "__socials"}>
-      <a href="#"><img src={gh} alt="GitHub Logo" /></a>
-      <a href="#"><img src={tw} alt="Twitter Logo" /></a>
-      <a href="#"><img src={ln} alt="LinekIn Logo" /></a>
+      {
+        items.map((item, i) => <a key={i} href={item.url}><img src={item.src} alt={item.alt} /></a>)
+      }
     </div>
   )
 }
