f6ce79d0334efe13d07943f40adad01fceca6620
vikspan <viktorija.span@gmail.com>
2025-08-06 12:11:01 +0200

    modify Header and add footer

--- src/js/Components/Header/Header.js
+++ src/js/Components/Header/Header.js
@@ -1,33 +1,10 @@
 import React from "react";
-import Logo from "./Logo/Logo";
-import Menu from "./Menu/Menu";
 import "./_header.scss";
-import Socials from "./Socials/Socials";
-import gh from "../../../assets/github.svg";
-import tw from "../../../assets/twitter.svg";
-import ln from "../../../assets/linkedin.svg";
 
-const Header = ({ type }) => {
+const Header = (props) => {
   return (
-    <div className={type}>
-      <Logo type={type + "__logo"} />
-      <div className={type + "__nav"}>
-        <Menu type={type + "__nav"}
-          items={
-            [
-              { title: "Home", url: "#" },
-              { title: "About", url: "#about" },
-              { title: "Tech Stack", url: "#stack" },
-              { title: "Projects", url: "#projects" },
-              { title: "Contact", url: "#contact" }
-            ]
-          } />
-        <Socials type={type + "__nav"} items={[
-          {url: "#", src: gh, alt: "GitHub"},
-          {url: "#", src: tw, alt: "Twitter"},
-          {url: "#", src: ln, alt: "LinkedIn"}
-        ]} />
-      </div>
+    <div className={props.type}>
+      {props.children}
     </div>
   )
 }
--- src/js/Pages/Home.js
+++ src/js/Pages/Home.js
@@ -2,6 +2,11 @@ import React from "react";
 import Header from "../Components/Header/Header";
 import Banner from "../Components/Banner/Banner";
 import Section from "../Components/Section/Section";
+import Projects from "../Components/Projects/Projects";
+import Socials from "../Components/Header/Socials/Socials";
+import Logo from "../Components/Header/Logo/Logo";
+import Menu from "../Components/Header/Menu/Menu";
+
 import html from "../../assets/html.svg"
 import css from "../../assets/css.svg"
 import js from "../../assets/js.svg"
@@ -10,15 +15,38 @@ import sass from "../../assets/sass.svg"
 import git from "../../assets/git.svg"
 import code from "../../assets/code.svg"
 import gith from "../../assets/gith.svg"
-import Projects from "../Components/Projects/Projects";
-import Socials from "../Components/Header/Socials/Socials";
-
+import gh from "../../assets/github.svg";
+import tw from "../../assets/twitter.svg";
+import ln from "../../assets/linkedin.svg";
 
 const Home = () => {
   return (
     <>
-      <Header type="header" />
+      <Header type="header">
+        <Logo type="header__logo" />
+
+        <div className="header__nav">
+          <Menu type="header__nav"
+            items={
+              [
+                { title: "Home", url: "#" },
+                { title: "About", url: "#about" },
+                { title: "Tech Stack", url: "#stack" },
+                { title: "Projects", url: "#projects" },
+                { title: "Contact", url: "#contact" }
+              ]
+            } />
+
+          <Socials type="header__nav" items={[
+            { url: "#", src: gh, alt: "GitHub" },
+            { url: "#", src: tw, alt: "Twitter" },
+            { url: "#", src: ln, alt: "LinkedIn" }
+          ]} />
+        </div>
+      </Header>
+
       <Banner />
+
       <Section type="tech" title="My Tech Stack" subtitle="Technologies I've been working with recently">
         <Socials type="tech" items={[
           { url: "#", src: html, alt: "HTML" },
@@ -31,10 +59,41 @@ const Home = () => {
           { url: "#", src: gith, alt: "GitHub" }
         ]} />
       </Section>
+
       <Section type="projects" title="Projects" subtitle="Things I've built so far">
         <Projects />
       </Section>
-      <Header type="footer" />
+
+      <Header type="footer">
+        <Logo type="footer__logo" />
+        <div className="footer__nav">
+          <Menu type="footer__nav"
+            items={
+              [
+                { title: "+91 12345 6789", url: "tel:+91 12345 6789" },
+                { title: "info@example.com", url: "mailto:info@example.com" }
+              ]
+            } />
+          <Socials type="footer__nav" items={[
+            { url: "#", src: gh, alt: "GitHub" },
+            { url: "#", src: tw, alt: "Twitter" },
+            { url: "#", src: ln, alt: "LinkedIn" }
+          ]} />
+        </div>
+      </Header>
+      <hr className="footer__divider" />
+      <Header type="footer">
+        <Menu type="footer__nav"
+          items={
+            [
+              { title: "+91 12345 6789", url: "tel:+91 12345 6789" },
+              { title: "info@example.com", url: "mailto:info@example.com" }
+            ]
+          } />
+        <div className="footer__copyright">
+          <p>Designed and built by <span className="gradient">Pavan MG</span> with <span className="gradient">Love</span> & <span className="gradient">Coffee</span></p>
+        </div>
+      </Header>
     </>
   )
 }
