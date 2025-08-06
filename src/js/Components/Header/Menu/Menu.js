71867f89b3e7f01d6c63c6ae07a6e61f11ada1a2
vikspan <viktorija.span@gmail.com>
2025-08-06 14:26:57 +0200

    add rwd menu

--- src/js/Components/Header/Menu/Menu.js
+++ src/js/Components/Header/Menu/Menu.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./_menu.scss";
+import menu from "../../../../assets/menu.svg";
 
 const Menu = ({ items }) => {
+  const [mobile, setMobile] = useState(true);
+
+  useEffect(() => {
+    if (window.innerWidth < 900) {  // TODO
+      setMobile(false);
+    }
+
+  }, []);
+
+  const toggleMobile = () => {
+    setMobile(prev => !prev);
+  }
+
   return (
     <nav className="menu">
-      <ul>
+      <img onClick={toggleMobile} src={menu} className="menu__toggle--icon" alt="mobile toggle" />
+      <ul style={{visibility: mobile? "visible" : "hidden"}}>
         {items.map((item, i) => (
           <li key={i}>
             <a href={item.url}>{item.title}</a>
