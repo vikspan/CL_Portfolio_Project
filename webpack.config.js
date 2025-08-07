8a161da48d4756513068158b444e2bc808ccc152
vikspan <viktorija.span@gmail.com>
2025-08-07 09:32:57 +0200

    update webpack.config for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     devtool: "inline-source-map",
     output: {
         filename: "out.js",
-        path: path.resolve(__dirname, `${entryPath}/build`),
+        path: path.resolve(__dirname, 'build'),
         clean: true,
     },
     devServer: {
